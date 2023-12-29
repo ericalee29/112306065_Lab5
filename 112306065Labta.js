ad31884d7c591b44322c230b766cc6d8e31795df
ericalee29 <147122740+ericalee29@users.noreply.github.com>
2023-12-29 12:37:11 +0800

    112306065_Lab5
    commit!

---
+++ 112306065Labta.js
@@ -0,0 +1,41 @@
+// Global variables
+let secretNumber = generateRandomNumber();
+
+// 4. Function to generate a random number between 1 and 10
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+
+function checkGuess() {
+  let userGuess = parseInt(document.getElementById("userGuess").value);
+  let resultElement = document.getElementById("result");
+
+  if (userGuess === secretNumber) {
+    resultElement.textContent = "Congratulations! You guessed the correct number!";
+    showImageAndPlaySound('picture2.jpeg', 'music2.mp3');
+    secretNumber = generateRandomNumber(); // Generate a new random number for the next round
+  } else {
+    resultElement.textContent = "Sorry, try again!";
+    showImageAndPlaySound('picture1.jpeg', 'music1.mp3');
+  }
+
+  // Note: Move this line inside the 'if-else' block to clear the input only after checking the guess
+  document.getElementById("userGuess").value = "";
+}
+
+
+// Function to show image and play sound
+function showImageAndPlaySound(imageSource, soundSource) {
+  let imageElement = document.createElement("img");
+  imageElement.src = imageSource;
+
+  let soundElement = document.createElement("audio");
+  soundElement.src = soundSource;
+  soundElement.autoplay = true;
+
+  let resultContainer = document.getElementById("result");
+  resultContainer.innerHTML = "";
+  resultContainer.appendChild(imageElement);
+  resultContainer.appendChild(soundElement);
+}
